39a2aba293bd8da112f30f7cf3410aac7bae21f7
Ssemakula-Jonathan <uhurussemakula069@gmail.com>
2021-06-19 11:46:32 +0300

    my project

---
+++ android/app/src/main/kotlin/com/example/introduction4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.introduction4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+       title: Text('NATIONAL ID CARD',
+         style: TextStyle(
+           fontSize: 20.0,
+           fontWeight: FontWeight.bold,
+           fontFamily: 'Vioda',
+         ),
+       ),
+       centerTitle: true,
+     ),
+
+
+      body: Column(
+        children: [
+          SizedBox(height: 10.0),
+
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/space2.jpg'),
+            radius: 70.0,
+          ),
+
+          Divider(
+            height: 20.0,
+            color: Colors.grey[600],
+          ),
+
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'SURNAME',
+            ),
+          ),
+          SizedBox(height: 10.0),
+
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'GIVEN NAME',
+            ),
+          ),
+          SizedBox(height: 10.0),
+
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'NATIONALITY',
+            ),
+          ),
+          SizedBox(height: 10.0),
+
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'SEX',
+            ),
+          ),
+          SizedBox(height: 10.0),
+
+          ElevatedButton(
+            onPressed: (){},
+            child: Text('SUBMIT'),
+
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+
